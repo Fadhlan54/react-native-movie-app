55eea2c0bb771e3bb99171ff4e419e569e14f4c7
Fadhlan54 <muhammadfadhlan0011@gmail.com>
2024-07-01 20:50:11 +0700

    develop_home_screen_layout

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,34 +1,42 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text, Button } from 'react-native'
+import { API_URL, API_ACCESS_TOKEN } from '@env'
 
-type RootStackParamList = {
-  Home: undefined
-  MovieDetail: undefined
-}
+const MovieDetail = () => {
+  const fetchData = (): void => {
+    if (API_URL == null || API_ACCESS_TOKEN.length == null) {
+      throw new Error('ENV not found')
+    }
 
-type MovieDetailScreenNavigationProp = NativeStackNavigationProp<
-  RootStackParamList,
-  'MovieDetail'
->
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    }
 
-const MovieDetail = (): JSX.Element => {
-  const navigation = useNavigation<MovieDetailScreenNavigationProp>()
+    fetch(API_URL, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Movie Detail Screen</Text>
-      <Button title="Kembali" onPress={() => navigation.navigate('Home')} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Movie Detail Page</Text>
+      <Button
+        title="Fetch Data"
+        onPress={() => {
+          fetchData()
+        }}
+      />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
 
 export default MovieDetail
